eef629830ff040fe0251e95675c4c7d4e397fcb1
Sangji Lee <sangllee20@gmail.com>
2022-08-01 19:35:53 +0900

    feat: add interface

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -9,9 +9,21 @@ declare module "*.woff";
 declare module "postcss-nesting";
 declare module "react-query";
 declare module "react-query/devtools";
-declare module "react-uuid";
+
+interface Hotel {
+  hotel_name: string;
+  occupancy: {
+    base: 2;
+    max: 2 | 3 | 4 | 5 | 6;
+  };
+  rating: number;
+  price: number;
+  review: number;
+  image: "https://source.unsplash.com/random";
+}
+
 interface LayoutProps {
   children: React.ReactNode;
 }
 
-export { LayoutProps };
+export { Hotel, LayoutProps };
