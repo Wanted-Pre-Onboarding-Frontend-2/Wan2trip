851a0486f8f9678208633bb2a0fdd71941337d9e
matty255 <89088205+matty255@users.noreply.github.com>
2022-08-01 11:03:22 +0900

    feat: getDate

--- src/components/calender/Cell.tsx
+++ src/components/calender/Cell.tsx
@@ -1,5 +1,7 @@
 import React from "react";
-
+import { DayState } from "../../store/global";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { setDate } from "date-fns";
 type CellType = {
   children: React.ReactNode | React.ReactNode[];
   value: number;
@@ -7,10 +9,14 @@ type CellType = {
 };
 
 const Cell = (props: CellType) => {
-  console.log(props.value);
+  // console.log(props.value);
+  const today = useRecoilValue(DayState);
+  const day = setDate(today, props.value);
   return (
     <>
-      <div className={props.className}>{props.children}</div>
+      <div className={props.className} onClick={() => console.log(day)}>
+        {props.children}
+      </div>
     </>
   );
 };
