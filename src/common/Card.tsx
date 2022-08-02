15962d510e37a800520ca78a4d2ae35206f3db58
dev-redo <mis05041@naver.com>
2022-08-02 18:05:35 +0900

    refactor: Virtaul scroll이 모든 컴포넌트에 대해 적용시킬 수 있게끔 리펙토링
    이전의 virtual scroll 컴포넌트는 Card를 전달받아 mapping하는 방식으로 진행했다.
    이렇게 될 시 Virtual scroll 컴포넌트는 해당 Card에 대 종속되게 된다.
    따라서 children으로 Card mapping을 전달한 후 virtual scroll에서 slice하는 방식으로 진행함으로써
    모든 컴포넌트에 대해 적용할 수 있도록 종속성을 제거해주었다.

--- src/common/Card.tsx
+++ src/common/Card.tsx
@@ -1,12 +1,7 @@
 import React from "react";
 import { Hotel } from "types/hotel";
 
-// TODO: Card는 모바일일 때 height가 더 큼
-// 1. 브라우저 크기에 따라 px 계산 (ResizeObserver 이용)
-//    - Card 바깥에 계산 hook 정의 후 사용
-//    - Result 페이지에서 해당 함수를 import해서 Virtual scroll에서 Card height 전달받게 구현
-// 2. Virtual Scroll Dynamic height 설정
-const Card = ({ item }: { item: Hotel }) => {
+const Card = ({ hotel }: { hotel: Hotel }) => {
   return (
     <>
       <div
@@ -16,7 +11,7 @@ const Card = ({ item }: { item: Hotel }) => {
           margin: "10px",
         }}
       >
-        {JSON.stringify(item.hotel_name)}
+        {JSON.stringify(hotel.hotel_name)}
       </div>
     </>
   );
