d043cf33ff31f3997eb31054f0e249b874b6c9d7
dev-redo <mis05041@naver.com>
2022-08-02 09:59:49 +0900

    feat: routing시 scroll to top 기능 구현

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,23 +1,43 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React, { useLayoutEffect } from "react";
+import {
+  BrowserRouter,
+  LayoutRouteProps,
+  Route,
+  Routes,
+  RoutesProps,
+  useLocation,
+} from "react-router-dom";
 import Home from "../page/Home";
 import Result from "../page/Result";
 import BookedList from "../page/BookedList";
 import { RecoilRoot } from "recoil";
 import Calender from "page/Calendar";
 
+// TODO: children type 지정
+const ScrollToTop = ({ children }: any) => {
+  const location = useLocation();
+
+  useLayoutEffect(() => {
+    document.documentElement.scrollTo(0, 0);
+  }, [location.pathname]);
+
+  return children;
+};
+
 const Router = () => {
   return (
     <BrowserRouter>
       <RecoilRoot>
-        <Routes>
-          {["/", "/home", "*"].map((path) => {
-            return <Route path={path} element={<Home />} key={path} />;
-          })}
-          <Route path="/result" element={<Result />} />
-          <Route path="/booked" element={<BookedList />} />
-          <Route path="/cal" element={<Calender />} />
-        </Routes>
+        <ScrollToTop>
+          <Routes>
+            {["/", "/home", "*"].map((path) => {
+              return <Route path={path} element={<Home />} key={path} />;
+            })}
+            <Route path="/result" element={<Result />} />
+            <Route path="/booked" element={<BookedList />} />
+            <Route path="/cal" element={<Calender />} />
+          </Routes>
+        </ScrollToTop>
       </RecoilRoot>
     </BrowserRouter>
   );
