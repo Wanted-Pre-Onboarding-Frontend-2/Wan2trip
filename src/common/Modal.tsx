ff3f04989e21617c4055852f6a3225b4ed7b60e5
matty255 <89088205+matty255@users.noreply.github.com>
2022-08-03 17:42:24 +0900

    layout: transparent modals

--- src/common/Modal.tsx
+++ src/common/Modal.tsx
@@ -58,12 +58,12 @@ export const Backdrop = tw.div`
 `;
 
 export const StyledModal = tw.div`
-  z-50 bg-white relative mx-auto rounded-md
+  z-50  relative mx-auto rounded-md bg-white md:bg-transparent
   w-full h-full max-w-[58.9rem] md:h-fit top-0 bottom-3
 `;
 
 export const Header = tw.div`
-flex justify-between p-1
+flex justify-between p-1 bg-white md:bg-transparent
 `;
 
 export const HeaderText = tw.div`
