53ec851448ddb2650eec5023e106fd83d37ec1a9
minkim <manjyuriah01@gmail.com>
2022-08-03 11:42:24 +0900

    conflict 해결

--- src/components/bookedList/BookedList.tsx
+++ src/components/bookedList/BookedList.tsx
@@ -1,3 +1,8 @@
+import React from "react";
+import { Hotel } from "types/types";
+import Card from "../Card";
+import Noreserve from "../../static/image/Noreserve.png";
+import VirtualScroll from "common/VirtualScroll";
 import BookedWeb from "./BookedWeb";
 import BookedTablet from "./BookedTablet";
 
