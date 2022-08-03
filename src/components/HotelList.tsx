5a2d51a965b67ffc30e81b88a90c30f9896695c5
dev-redo <mis05041@naver.com>
2022-08-03 17:57:40 +0900

    feat: card 리스트 virtual scroll 적용 및 로딩중 컴포넌트 생성

--- src/components/HotelList.tsx
+++ src/components/HotelList.tsx
@@ -1,24 +1,29 @@
-import React from "react";
-import Card from "../components/Card";
+import React, { Suspense } from "react";
+import { uid } from "react-uid";
 
+import Card from "../components/Card";
+import VirtualScroll from "common/VirtualScroll";
 import { Hotel } from "types/types";
 import { useGetHotels } from "api/queries";
 
+// TODO: Suspense 도입 여부 묻기
+// -> 일단 isLoading으로 해둔 상태 (깜빡거리는 현상 발생)
 const HotelList = () => {
-  const { data: hotels } = useGetHotels();
+  const { data: hotels, isLoading } = useGetHotels();
+
+  if (isLoading) {
+    return <div>로딩중...</div>;
+  }
 
   return (
     <>
       <div>홈</div>
-      {hotels
-        ?.map((hotel: Hotel) => {
-          return (
-            <div key={hotel.hotel_name} className="w-full">
-              <Card data={hotel} />
-            </div>
-          );
-        })
-        .slice(0, 20)}
+
+      <VirtualScroll itemHeight={20} columnGap={0.625}>
+        {hotels.map((hotel: Hotel, index: number) => (
+          <Card key={uid(index)} data={hotel} />
+        ))}
+      </VirtualScroll>
     </>
   );
 };
