85f81d1da4c677480823559c712028f0ac47b28b
Sangji Lee <sangllee20@gmail.com>
2022-08-04 17:08:48 +0900

    style: WIP

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -3,17 +3,21 @@ import Header from "../components/layout/Header";
 import SearchBar from "../components/search/SearchBar";
 import BackHeader from "../components/layout/BackHeader";
 import Layout from "../components/layout/Layout";
-import tw from "tailwind-styled-components";
-import Card from "../common/Card";
-import { Link } from "react-router-dom";
-import HotelList from "../components/HotelList";
+import Background from "../static/image/background.jpg";
+
+const HomeStyle = {
+  backgroundImage: "url(" + { Background } + ")",
+};
+
 const Home = () => {
   return (
     <Layout>
-      <Header />
-      <SearchBar />
-      <div className="flex flex-col items-center justify-center w-full gap-10 mx-auto md:pt-16 md:mt-44">
-        <HotelList />
+      <div style={HomeStyle}>
+        <img src={Background} alt="" />
+        <Header />
+        <div>Play, Share, Stay 트립비토즈</div>
+        <SearchBar />
+        {/* <div className="flex flex-col items-center justify-center w-full gap-10 mx-auto md:pt-16 md:mt-44"></div> */}
       </div>
     </Layout>
   );
