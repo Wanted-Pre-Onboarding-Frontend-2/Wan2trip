4180de0ef1d62d1f7f751edb5db87608be8fee7e
Sangji Lee <sangllee20@gmail.com>
2022-08-05 16:58:58 +0900

    refactor

--- src/hooks/useConfirm.tsx
+++ src/hooks/useConfirm.tsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import { dateArray, DayState, ModalState } from "../store/global";
-import { useRecoilState } from "recoil";
 
 export const useConfirm = () => {
   const [isShown, setIsShown] = useState<boolean>(false);
